f36eff050f1beada25e6b4c68d4936132cd2832e
Joydeep Nath <joydeepnath279@gmail.com>
2025-11-30 19:36:00 +0530

    add constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const mongoString =
+  "mongodb+srv://JoydeepNath279:NiDoQXh2Hjyvlmbk@devtinder.i2nxfgd.mongodb.net/DevTinder";
+
+const jwtPrivateKey = "Joydeep@279";
+module.exports = { mongoString, jwtPrivateKey };
